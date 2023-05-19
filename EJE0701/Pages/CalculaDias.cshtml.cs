d5bddc029ab680d4c129b562f644f6d2929d72ef
Samuel <samudawb@gmail.com>
2023-05-19 17:36:00 +0100

    first commit

---
+++ EJE0701/Pages/CalculaDias.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EJE0701.Pages
+{
+    public class CalculaDiasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ EJE0701/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EJE0701
+{
+
+    [IgnoreAntiforgeryToken(Order = 1001)]
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+}
