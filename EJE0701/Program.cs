cd8f977387d469438d4564aa618481a9904e5628
SamuDaw <samudawb@gmail.com>
2023-05-20 21:02:48 +0100

    Finalizado

--- EJE0701/Program.cs
+++ EJE0701/Program.cs
@@ -3,5 +3,6 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 app.MapRazorPages();
+app.UseStaticFiles();
 
 app.Run();
--- backup/CalculaDias.cshtml.cs
+++ backup/CalculaDias.cshtml.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace EJE0701.Pages
